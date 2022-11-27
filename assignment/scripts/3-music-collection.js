43f74ec7b697d14ad396eb21875bdb48f81e117b
henrisoho <bakingwithmymother@gmail.com>
2022-11-27 09:16:18 -0600

    created function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,17 @@
 console.log('***** Music Collection *****')
+let collection = [];
+let Album = {
+
+}
+
+function addToCollection(title, artist, yearPublished){
+    Album.title = title;
+    Album.artist = artist
+    Album.yearPublished = yearPublished
+    collection.push(Album)
+    return collection
+}
+
+addToCollection('Smooth', 'Henri', 1999)
+
+console.log(collection)
