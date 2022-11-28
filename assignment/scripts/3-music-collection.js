081524dee683c957e767fd27a1841acff4d0b492
henrisoho <bakingwithmymother@gmail.com>
2022-11-27 20:59:29 -0600

    functi0on im so lost

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -12,6 +12,19 @@ function addToCollection(title, artist, yearPublished){
     return collection
 }
 
-addToCollection('Smooth', 'Henri', 1999)
 
+addToCollection('Smooth', 'Henri', 1999);
+addToCollection('new wave', 'Henri', 2007);
+
+addToCollection('blue jeans', 
+'lana del ray', 2012)
+addToCollection('Dream GHirl', 
+'Kolohe Kai', 2009)
+addToCollection('Move your body', 
+'Own Boss', 2021)
+addToCollection('Call me', 
+'blonde', 1980)
+addToCollection('cocaine model', 
+'ZHU', 2014)
 console.log(collection)
+
