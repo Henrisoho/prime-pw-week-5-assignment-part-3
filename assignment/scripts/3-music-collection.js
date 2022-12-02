bd50cc50de12e43393a2921398a56197c27d7806
henrisoho <bakingwithmymother@gmail.com>
2022-12-02 01:36:21 -0600

    finito

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,30 +1,45 @@
 console.log('***** Music Collection *****')
 let collection = [];
-let Album = {
-
-}
 
 function addToCollection(title, artist, yearPublished){
-    Album.title = title;
-    Album.artist = artist
-    Album.yearPublished = yearPublished
-    collection.push(Album)
-    return collection
+   let newAlbum = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished,
+   }
+    collection.push(newAlbum);
+    return newAlbum
 }
 
 
 addToCollection('Smooth', 'Henri', 1999);
-addToCollection('new wave', 'Henri', 2007);
+addToCollection('Sarah', 'Graves', 2021);
+addToCollection('Model', 'Zhu', 2012);
+addToCollection('New York', 'Frank Sinatra', 1979);
+addToCollection('City', 'Atlas', 2019);
+addToCollection('paper moon', 'Nat', 1985);
 
-addToCollection('blue jeans', 
-'lana del ray', 2012)
-addToCollection('Dream GHirl', 
-'Kolohe Kai', 2009)
-addToCollection('Move your body', 
-'Own Boss', 2021)
-addToCollection('Call me', 
-'blonde', 1980)
-addToCollection('cocaine model', 
-'ZHU', 2014)
-console.log(collection)
+//console.log(collection)
 
+    function showCollection(Arr){
+  console.log(`There are ${Arr.length} Albums`);
+  for (let i in Arr){
+    console.log(`${Arr[i].title} by ${Arr[i].artist} published in ${Arr[i].yearPublished}`)
+  }
+    }
+
+console.log(showCollection(collection));
+
+
+
+function findByArtist(artist){
+  let Result = [];
+  for (let i in collection){
+    if (artist == collection[i].artist){
+    Result.push(collection[i]);
+    }
+  }
+  return Result;
+}
+
+console.log(findByArtist('Henri'));
